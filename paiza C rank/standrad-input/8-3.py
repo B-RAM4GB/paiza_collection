88647538c546d2f8d4661baa2c5281b920d745c2
B-RAM4GB <ruka24285@gmail.com>
2026-05-27 12:06:50 +0900

    Update 8-3.py

--- paiza C rank/standrad-input/8-3.py
+++ paiza C rank/standrad-input/8-3.py
@@ -1,6 +1,6 @@
 le,ri=input().split(".")
 ri=ri.ljust(4,"0")   #文字列.ljust(幅,埋める文字) mean:左寄せにして指定した幅になるまで右側を埋める。例:a.ljust(2,"0")→a0
-if int (ri[3])==5:
+if int (ri[3])==5:    #rjust:右寄せ　　　ljustのところにrjust入れるだけでいい
     num=int(ri[:3])+1
 else:
     num=int(ri[:3])

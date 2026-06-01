9227aeb0b00884b76ca7a32e8a9dcde065d88eed
B-RAM4GB <ruka24285@gmail.com>
2026-06-01 21:53:15 +0900

    Create 10-5.py

---
+++ paiza C rank/standrad-input/10-5.py
@@ -0,0 +1,16 @@
+h,w,a,b=map(int,input().split())
+a=str(a).rjust(9," ")
+b=str(b).rjust(9," ")
+for i in range(h):
+    for k in range(w):
+        if k==w-1:
+            print(f"({a},{b})")
+            if i==h-1:
+                pass
+            else:
+                rl=(21*w+3*(w-1))
+                print("="*rl)
+            
+        else:
+            print(f"({a},{b})",end=" | ")
+            

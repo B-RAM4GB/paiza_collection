314b2b3cef367bbadb90dac9c8e31800e4733a7a
B-RAM4GB <ruka24285@gmail.com>
2026-06-03 11:05:32 +0900

    Update 10-5.py

--- paiza C rank/standrad-input/10-5.py
+++ paiza C rank/standrad-input/10-5.py
@@ -13,4 +13,21 @@ for i in range(h):
             
         else:
             print(f"({a},{b})",end=" | ")
+#==================================================
+h,w,a,b=map(int,input().split())
+a=str(a).rjust(9," ")
+b=str(b).rjust(9," ")
+for i in range(h):
+    for k in range(w):
+        if k==w-1:
+            print(f"({a}, {b})")
+            if i==h-1:
+                pass
+            else:
+                rl=(22*w+3*(w-1))
+                print("="*rl)
+            
+        else:
+            print(f"({a}, {b})",end=" | ")
+#success
             

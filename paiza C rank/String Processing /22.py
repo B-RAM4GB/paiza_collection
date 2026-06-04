d496597b863ad0f66ca26cb7c32a7fa36c896ef0
B-RAM4GB <ruka24285@gmail.com>
2026-06-04 11:35:40 +0900

    Create 22.py

---
+++ paiza C rank/String Processing /22.py
@@ -0,0 +1,16 @@
+s=input()
+
+a=s.isdigit()
+if a==True:
+    print("YES")
+else:
+    print("NO")
+
+'''
+十進数字か判定: str.isdecimal()
+数字か判定: str.isdigit()
+数を表す文字か判定: str.isnumeric()
+英字か判定: str.isalpha()
+英数字か判定: str.isalnum()
+ASCII文字か判定: str.isascii()
+'''

3e7050f5e709a731d61f984a7cdec2306ce6ef9f
B-RAM4GB <ruka24285@gmail.com>
2026-06-11 10:09:10 +0900

    Update 01-3.py

--- paiza-B-rank/classes-and-structs/01-3.py
+++ paiza-B-rank/classes-and-structs/01-3.py
@@ -3,7 +3,7 @@ list_n=[]
 for i in range(n):
     n1,o,b,s=input().split()
     list_n.append([n1,int(o),b,s])
-list_n.sort(key=lambda x: x[1])   #sortはリストの中身を並び替えてくれる。
-for k in list_n:
+list_n.sort(key=lambda x: x[1])   #sort(デフォルトでは小さい順)はkeyに従ってリストの中身を並び替えてくれる。lambda x: x[1]はリストの一番目の要素を取り出す関数。
+for k in list_n:                  #もし大きい順にしたいのであれば(key=lambda x: x[1], reverse=True)とする。
     print(k [0], k[1], k[2], k[3])
         

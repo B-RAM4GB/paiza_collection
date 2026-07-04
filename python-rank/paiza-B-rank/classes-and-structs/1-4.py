1160c62bdff434c336f0a95293c6855acaf56603
B-RAM4GB <ruka24285@gmail.com>
2026-07-05 00:55:10 +0900

    Create 1-4.py

---
+++ python-rank/paiza-B-rank/classes-and-structs/1-4.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, o, b, s):
+        self.name = name
+        self.o = o
+        self.b = b
+        self.s = s
+
+n, k = map(int, input().split())
+
+roster = [None] * n
+
+for i in range(n):
+    name, o, b, s = input().split()
+    roster[i] = Student(name, o, b, s)
+
+for _ in range(k):
+    a, new_name = input().split()
+    a = int(a)
+    roster[a - 1].name = new_name
+
+
+for stu in roster:
+    print(stu.name, stu.o, stu.b, stu.s)

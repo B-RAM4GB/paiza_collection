87b8532abd5a7e072ad546186909266a3ef3968a
B-RAM4GB <ruka24285@gmail.com>
2026-07-08 16:29:20 +0900

    Update 2-1.py

--- python-rank/paiza-B-rank/classes-and-structs/2-1.py
+++ python-rank/paiza-B-rank/classes-and-structs/2-1.py
@@ -1,10 +1,34 @@
 class employee:
-    def _init_(self, number, name):
+    def __init__(self, number, name):
         self.number=number
         self.name=name
         
-    def getnum(){
-    return number;
-}
+    def getnum(self):  #selfを使いますよということ。クラスの中でメソッドを作るときはこれを書く
+        return self.number
+    
+    def getname(self):
+        return self.name
+        
+        
+lst=[]
+        
 n=int(input())
-
+for i in range(n):
+    s=input()
+    S=s.split()
+    cad=S[0]
+    
+    if cad=="make":
+        new_lst=employee(S[1], S[2])
+        lst.append(new_lst)
+        
+    elif cad == "getnum":
+        index = int(S[1]) - 1  # 何番目か（インデックス）を計算
+        emp = lst[index]       # リストから社員を取り出す
+        print(emp.getnum())    # その社員の番号を表示する
+        
+    elif cad == "getname":
+        index = int(S[1]) - 1
+        emp = lst[index]
+        print(emp.getname())   # その社員の名前を表示する
+    
